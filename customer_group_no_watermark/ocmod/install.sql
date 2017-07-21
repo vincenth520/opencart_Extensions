0b87a93efadd5f91145fe585e0cf719617a09caf
vincenth520 <1091986039@qq.com>
2017-07-21 10:50:40 +0800

    update readme

--- customer_group_no_watermark/ocmod/install.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE  `oc_customer_group_description`  ADD  `no_watermark_num` int NOT NULL default 0;
-ALTER TABLE  `oc_customer`  ADD  `no_watermark_num` int NOT NULL default 0;
-ALTER TABLE  `oc_customer`  ADD  `no_watermark_clear_month` int NOT NULL default 0;
-ALTER TABLE  `oc_customer`  ADD  `nowatermark` tinyint NOT NULL default 0;
-CREATE TABLE `oc_download_no_watermark`(id int primary key auto_increment,product_id int,customer_id int);
